332cf929fd538b5a558a55bc2a73a28daf2e639e
Christian Kyony <ckyony@changamuka.com>
2017-02-05 14:00:21 +0200

    Edit Readme.adoc

--- lib/nexpose-security-console.rb
+++ lib/nexpose-security-console.rb
@@ -1,4 +1,5 @@
 require 'nexpose-security-console/version.rb'
+# require 'nexpose-security-console/cli.rb'
 
 # Add requires for other files you add to your project here, so
 # you just need to require this one file in your bin file
